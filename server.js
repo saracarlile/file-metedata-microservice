d98f66463f9b768d868c37aeb21ce49cc259d917
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2017-03-13 11:44:28 -0500

    change post path

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 8080;
 app.use(express.static(__dirname + "/public")); //server index.html
 
  
-app.post('/send', upload.single('avatar'), function (req, res, next) {
+app.post('/get-file-size', upload.single('uploadFile'), function (req, res, next) {
   // req.file is the `avatar` file 
   // req.body will hold the text fields, if there were an
   return res.json({ "file-size": req.file.size});
